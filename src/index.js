4575af976fcae85eb86760b8922811366bb87f40
hemantlakhotia204 <hemantlakhotia204@gmail.com>
2023-07-24 17:44:37 +0530

    test

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import reportWebVitals from "./reportWebVitals";
 import { FwAvatarC as FwAvatar, FwAvatarGroupC as FwAvatarGroup, TestView } from "./views/test_component";
 
 let users = [
-  { name: "Richard", company: "PiedPiper" },
+  { name: "Surya", company: "PiedPiper" },
   { name: "Dinesh", company: "PiedPiper" },
   { name: "Hemant", company: "PiedPiper" },
 ];
